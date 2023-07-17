c0df882c25b673500a2de06245ec7739a7ca88cc
SEAME-Workstation11 <gdh1685@kookmin.ac.kr>
2023-07-17 23:36:16 +0200

    update

--- dko/Module04/Test_fail/car.cpp
+++
@@ -1,8 +0,0 @@
-#include "car.h"
-
-Car::Car()
-{
-    x = 0;
-    y = 0;
-    //float x,y,speed,angle; int n;
-}
--- dko/Module04/Test_fail/game.cpp
+++
@@ -1,9 +0,0 @@
-#include "game.h"
-
-#include <QKeyEvent>
-#include <QDebug>
-
-game::game() : m_timer(new QTimer(this))
-{
-    m_timer->start(1000.0f/60.0f);
-}
