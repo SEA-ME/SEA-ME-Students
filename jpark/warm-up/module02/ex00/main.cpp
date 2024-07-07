4551047e65c30fa55fd84c61644fe4361a4b1d6d
Clearsu <clearwater1219@gmail.com>
2024-07-07 18:37:41 +0200

    feat: add more smart pointer examples

--- jpark/warm-up/module02/ex00/main.cpp
+++ jpark/warm-up/module02/ex00/main.cpp
@@ -19,7 +19,38 @@ void danglingPointerExample() {
 }
 
 void smartPointerExample() {
-    std::shared_ptr<Car> ptr = std::make_shared<Car>("Kia", 80);
+    {
+        std::cout << "-----uniquePtr example-----" << std::endl;
+        std::unique_ptr<Car> uniquePtr = std::make_unique<Car>("Ford", 100);
+        std::cout << uniquePtr->getName() << std::endl;
+    }
+
+    {
+        std::cout << "-----sharedPtr example-----" << std::endl;
+        std::shared_ptr<Car> sharedPtr1 = std::make_shared<Car>("Kia", 80);
+        {
+            std::shared_ptr<Car> sharedPtr2 = sharedPtr1;
+            std::cout << "Shared pointer use count: " << sharedPtr1.use_count() << std::endl;
+        }
+        std::cout << "Shared pointer use count after scope: " << sharedPtr1.use_count() << std::endl;
+    }
+
+    {
+        std::cout << "-----weakPtr example-----" << std::endl;
+        std::shared_ptr<Car> sharedPtr1 = std::make_shared<Car>("Kia", 80);
+        std::weak_ptr<Car> weakPtr = sharedPtr1;
+
+        if (!weakPtr.lock()) {
+            std::cout << "The object has been destroyed." << std::endl;
+        }
+
+        std::cout << "Destroy object" << std::endl;
+        sharedPtr1.reset();
+
+        if (!weakPtr.lock()) {
+            std::cout << "The object has been destroyed." << std::endl;
+        }
+    }
 }
 
 int main() {
